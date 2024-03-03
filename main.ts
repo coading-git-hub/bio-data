0d30408813810544d5fe521f75e9961295f3f027
KIRAN AHMED <162012113+coading-git-hub@users.noreply.github.com>
2024-03-03 14:32:14 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,104 @@
+// data types with variables use let
+
+let fullName: string = "KIRAN"
+fullName = "KIRAN-AHMED"
+console.log(fullName);
+
+//my age
+let age: number = 40
+console.log(age);
+
+//my status
+let isStudent: boolean = true
+console.log(isStudent);
+
+//my education
+let education: string = "Graduate" 
+console.log(education);
+
+//my DOB
+let dateofBirth: number =  26-11-1980
+console.log(dateofBirth);
+
+//my dream
+let myDream: string = "Become a web-devolper"
+console.log(myDream);
+
+//my country
+let myCountry: string = "PAKISTAN"
+console.log(myCountry);
+
+// my city
+let myCity: string = "KARACHI"
+console.log(myCity);
+
+//my religion
+let myReligion: string = "ISLAM"
+console.log(myReligion);
+
+//my family members
+let familyMembers: number = 4
+console.log(familyMembers);
+ 
+//my merritual status
+const merried: boolean = true
+console.log(merried);
+ 
+//my favourate color
+let favourateColor: string = "ORANGE"
+console.log(favourateColor);
+
+//my favourite country
+let favourateCountry: string = "SAUDIARAB"
+console.log(favourateCountry);
+
+//my favourate month
+let favourateMonth: string = "RAMAZAN"
+console.log(favourateMonth);
+ 
+//my favourate poet
+let favouratePoet: string = "ALLAMA IQBAL"
+console.log(favouratePoet);
+
+//favourate dish
+let favourateDish: string = "FRIED QEEMA"
+console.log(favourateDish);
+
+//favourate places
+let favouratePlaces: string = "MAKKA-MADINA"
+console.log(favouratePlaces);
+
+//favourate weather
+let favourateWeather: string = "SPRING"
+console.log(favourateWeather);
+
+//favourate book
+let favourateBook: string =  "QURAN SHAREEF"
+console.log(favourateBook);
+
+//favourate game
+let favourateGame: string = "CRICKET"
+console.log(favourateGame);
+ 
+//favourate founder
+let favourateFounder: string = "QUAID-E-AZAM"
+console.log(favourateFounder);
+
+//total sisters
+let totalSisters: number = 5
+console.log(totalSisters);
+
+//total brother
+let totalBrother:number = 1
+console.log(totalBrother);
+
+//favourate dress
+let favourateDress: string = "PAKISTANI-DRESESS"
+console.log(favourateDress);
+
+//massege
+let message: string = "Respect your parants & always follow islamic rules"
+console.log(message);
+  
+/*thank GOD ihave done my home work
+ ALHAMDULLILAH*/
